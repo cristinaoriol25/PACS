848d53cee5cc460bd64e57eaa82a41512d071ae3
nestor98 <nestor98@github.com>
2021-10-06 12:07:03 +0200

    Todo en un fichero ya vale

--- lab1_materials/matrices/test_mult_methods.sh
+++ lab1_materials/matrices/test_mult_methods.sh
@@ -2,12 +2,17 @@
 sizes="50 100 200 500 700 800 1000 1200 1500 2000"
 executables="sequential my_eigen_matmult my_eigen_matmult_no_vect"
 
+echo "method N User(s) System(s) Real(s)" > "logs/comparison.log"
+
+
 for ex in $executables; do
   echo "Testing ${ex}..."
   for n in $sizes; do
     echo "Size ${n}..."
+    # echo -n "${ex} ${n} " >> "logs/${ex}.log" # en ficheros distintos
+    echo -n "${ex} ${n} " >> "logs/comparison.log"
     # {time -o "./${ex}" ${n}} 2> "logs/${ex}-${n}.log"
-    \time -f "%U %S %e" -o "logs/${ex}-${n}.log" "./${ex}" ${n}
+    \time -f "%U %S %e" -a -o "logs/comparison.log" "./${ex}" ${n}
   done
   echo "-----------------------"
 done
