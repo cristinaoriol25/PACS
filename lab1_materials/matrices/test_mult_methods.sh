46ac482eb886dc748fef8b84aa3386b0355f865f
nestor98 <nestormg98@gmail.com>
2021-10-13 19:04:27 +0200

    test mas veces

--- lab1_materials/matrices/test_mult_methods.sh
+++ lab1_materials/matrices/test_mult_methods.sh
@@ -1,6 +1,7 @@
 # TODO: time pasandoles distintos tamaños de matrices
 sizes="50 100 200 500 700 800 1000 1200 1500 2000"
 executables="sequential my_eigen_matmult my_eigen_matmult_no_vect"
+repeat=10
 
 echo "method N User(s) System(s) Real(s)" > "logs/comparison.log"
 
@@ -8,11 +9,13 @@ echo "method N User(s) System(s) Real(s)" > "logs/comparison.log"
 for ex in $executables; do
   echo "Testing ${ex}..."
   for n in $sizes; do
-    echo "Size ${n}..."
-    # echo -n "${ex} ${n} " >> "logs/${ex}.log" # en ficheros distintos
-    echo -n "${ex} ${n} " >> "logs/comparison.log"
-    # {time -o "./${ex}" ${n}} 2> "logs/${ex}-${n}.log"
-    \time -f "%U %S %e" -a -o "logs/comparison.log" "./${ex}" ${n}
+    for ((i=0; i<repeat; i++)); do
+      echo "Size ${n}..."
+      # echo -n "${ex} ${n} " >> "logs/${ex}.log" # en ficheros distintos
+      echo -n "${ex} ${n} " >> "logs/comparison.log"
+      # {time -o "./${ex}" ${n}} 2> "logs/${ex}-${n}.log"
+      \time -f "%U %S %e" -a -o "logs/comparison.log" "./${ex}" ${n}
+    done
   done
   echo "-----------------------"
 done
