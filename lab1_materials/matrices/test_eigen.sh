097bb558dc81456cd79bde1c1c0d593d2fcaffd3
nestor98 <nestormg98@gmail.com>
2021-10-24 10:29:43 +0200

    test eigen

---
+++ lab1_materials/matrices/test_eigen.sh
@@ -0,0 +1,22 @@
+# TODO: time pasandoles distintos tamaños de matrices
+sizes="2000 4000 8000 16000 50000 100000"
+executables="my_eigen_matmult my_eigen_matmult_no_vect"
+repeat=10
+
+echo "method N User(s) System(s) Real(s)" > "logs/comparison.log"
+
+out="comparison-eigen.log"
+
+for ex in $executables; do
+  echo "Testing ${ex}..."
+  for n in $sizes; do
+    for ((i=0; i<repeat; i++)); do
+      echo "Size ${n}..."
+      # echo -n "${ex} ${n} " >> "logs/${ex}.log" # en ficheros distintos
+      echo -n "${ex} ${n} " >> "logs/${out}"
+      # {time -o "./${ex}" ${n}} 2> "logs/${ex}-${n}.log"
+      \time -f "%U %S %e" -a -o "logs/${out}" "./${ex}" ${n}
+    done
+  done
+  echo "-----------------------"
+done
